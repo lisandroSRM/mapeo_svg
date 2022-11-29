7f20e692e4d3683228dd2c5b0ae4ff8ab72a7136
lisandroSRM <solislisandro9@gmail.com>
2022-11-28 21:13:24 -0600

    mapeo_svg

---
+++ header.php
@@ -0,0 +1,31 @@
+
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <style>
+        body{
+            background: pink;
+
+        }
+        .circulo{
+            width : 40%;
+            margin: auto;
+        }
+        .circulo path{ 
+            stroke: #5D11E3;
+            stroke-width:2px;
+        }
+        .circulo :hover{
+           fill: cadetblue ;
+           cursor: pointer;
+           stroke: pink;
+        }
+        
+        
+    </style>
+    <title>SVG</title>
+    
+</head>
+<body>
